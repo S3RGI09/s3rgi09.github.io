328c8d626ae8ff471faa6f1a0c4b1cbb1862e2ef
Sergio Casero Verdial <96842235+S3RGI09@users.noreply.github.com>
2024-02-16 16:34:21 +0100

    Update login.php

--- login.php
+++ login.php
@@ -4,8 +4,8 @@ session_start();
 // Verificar si el formulario de login fue enviado
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Verificar las credenciales (aquí deberías conectar con tu base de datos y verificar las credenciales)
-    $username = "usuario"; // Cambia esto por tu nombre de usuario
-    $password = "contraseña"; // Cambia esto por tu contraseña
+    $username = "sergio"; // Cambia esto por tu nombre de usuario
+    $password = "2009"; // Cambia esto por tu contraseña
 
     if ($_POST['username'] === $username && $_POST['password'] === $password) {
         // Las credenciales son válidas, iniciar sesión
